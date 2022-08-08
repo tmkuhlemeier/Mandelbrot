f16701eaba40c2494f9d97114c9804c6fdf7ed12
Thomas Kuhlemeier <83757635+tmkuhlemeier@users.noreply.github.com>
2022-08-08 13:40:23 +0200

    Add files via upload

---
+++ mandel/Mandelbrot.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mandelbrot
+{
+
+    partial class MandelForm
+    {
+        // membervariabelen
+        Bitmap tekening;
+        double schaal = 0.01;
+        double schaalfactor = 1;
+        double xklik = 0;
+        double yklik = 0;
+        double xmidden = 0;
+        double ymidden = 0;
+        double mandelgetalmax = 100;
+        Color kleur;
+        Color kleur1 = Color.Black;
+        Color kleur2 = Color.White;
+        Color kleur3 = Color.White;
+        Color kleur4 = Color.White;
+        Color kleur5 = Color.White;
+        Color kleur6 = Color.Black;
+        Color kleur7 = Color.Black;
+        Color kleur8 = Color.Black;
+
+
+        // mandelgetal berekenen
+        public double BerekenMandel(double xmandel, double ymandel)
+        {
+            double a, b;
+            double mandelgetal = 0; 
+            double mandelroot = 0; 
+            a = xmandel;
+            b = ymandel;
+
+            while ((mandelgetal <= mandelgetalmax) && (mandelroot <= 2))
+            {
+                double temp_a = a;
+                a = (a * a - b * b + xmandel);
+                b = (2 * temp_a * b + ymandel);
+                mandelroot = Math.Sqrt(a * a + b * b);
+                mandelgetal++;
+
+            }
+            return mandelgetal;
+        }
+
+
+        // pixels converteren naar mandelassenstelsel en bitmap tekenen
+        public void Tekenmandel(object obj, PaintEventArgs pea)
+        {
+            // koppeling van bitmap aan picturebox
+            tekening = new Bitmap(picturebox.Width, picturebox.Height);
+
+            for (int x = 0; x < picturebox.Width; x++)
+            {
+                for (int y = 0; y < picturebox.Height; y++)
+                {
+                    double xmandel = xmidden + schaal * (x - picturebox.Width/2);
+                    double ymandel = ymidden + schaal * (picturebox.Height/2 - y);
+
+                    if ((int)(BerekenMandel(xmandel, ymandel)) % 2 == 1)
+                        kleur = kleur1;
+                    else if ((int)(BerekenMandel(xmandel, ymandel)) % 3 == 1)
+                        kleur = kleur3;
+                    else if ((int)(BerekenMandel(xmandel, ymandel)) % 4 == 0)
+                        kleur = kleur4;
+                    else if ((int)(BerekenMandel(xmandel, ymandel)) % 5 == 1)
+                        kleur = kleur5;
+                    else if ((int)(BerekenMandel(xmandel, ymandel)) % 3 == 0)
+                        kleur = kleur6;
+                    else if ((int)(BerekenMandel(xmandel, ymandel)) % 5 == 0)
+                        kleur = kleur7;
+                    else if ((int)(BerekenMandel(xmandel, ymandel)) % 7 == 0)
+                        kleur = kleur8;
+                    else
+                        kleur = kleur2;
+
+                    tekening.SetPixel(x, y, kleur);
+                }
+            }
+
+            picturebox.Image = tekening;
+        }
+
+
+        // methode om te vergroten waar er geklikt wordt
+        public void Klikzoom(object sender, EventArgs mea)
+        {
+            xklik = (double)((System.Windows.Forms.MouseEventArgs)mea).X;
+            yklik = (double)((System.Windows.Forms.MouseEventArgs)mea).Y;
+
+             xmidden = xmidden + schaal * (xklik - picturebox.Width/2);
+             ymidden = ymidden + schaal * (picturebox.Height/2 - yklik);
+
+            try
+            {
+                schaalfactor = double.Parse(Schaalfactor.Text);
+
+            }
+            catch (Exception)
+            {
+            }
+
+            schaal = schaalfactor * schaal;
+
+            MiddenX.Text = xmidden.ToString();
+            MiddenY.Text = ymidden.ToString();
+            Schaal.Text = schaal.ToString();
+            Max.Text = mandelgetalmax.ToString();
+            Schaalfactor.Text = schaalfactor.ToString();
+
+            Invalidate();
+        }
+
+
+        // methode voor het klik event van de buttons OK en Reset
+        public void Knopklik_Click(object sender, System.EventArgs e)
+        {
+            if (sender == this.OK)
+            {
+                try
+                {
+                    // converteren van tekst uit tekstboxen naar doubles en deze toekennen aan de variabelen
+                    schaal = double.Parse(Schaal.Text);
+                    ymidden = double.Parse(MiddenY.Text);
+                    mandelgetalmax = double.Parse(Max.Text);
+                    xmidden = double.Parse(MiddenX.Text);
+                    schaalfactor = double.Parse(Schaalfactor.Text);
+                }
+                catch (Exception)
+                {
+                }
+                schaal = schaalfactor * schaal;
+
+            }
+
+            // deze zet de gegevens (excl. kleur) en schaal terug naar de beginstand
+            if (sender == this.Reset)
+            {
+                schaal = 0.01;
+                xmidden = 0;
+                ymidden = 0;
+                mandelgetalmax = 100;
+                schaalfactor = 0.5;
+            }
+
+            // converteren van de doubles van de variabelen naar strings en deze toekennen aan de tekstboxen
+            MiddenX.Text = xmidden.ToString();
+            MiddenY.Text = ymidden.ToString();
+            Schaal.Text = schaal.ToString();
+            Max.Text = mandelgetalmax.ToString();
+            Schaalfactor.Text = schaalfactor.ToString();
+
+            Invalidate();
+        }
+
+
+        // methode voor het klik event van de tekst in de combobox
+            public void Keuzelijst_SelectedIndexChanged(object sender, EventArgs evt)
+        {
+            string gekozenwaarde = ((ComboBox)sender).SelectedText;
+            switch (gekozenwaarde)
+            {
+                case "basis":
+
+                    schaal = 0.01;
+                    xmidden = 0;
+                    ymidden = 0;
+                    mandelgetalmax = 100;
+                    schaalfactor = 0.5;
+                    kleur1 = Color.Black;
+                    kleur2 = Color.White;
+                    kleur3 = Color.White;
+                    kleur4 = Color.White;
+                    kleur5 = Color.White;
+                    kleur6 = Color.Black;
+                    kleur7 = Color.Black;
+                    kleur8 = Color.Black;
+                    MiddenX.Text = xmidden.ToString();
+                    MiddenY.Text = ymidden.ToString();
+                    Schaal.Text = schaal.ToString();
+                    Max.Text = mandelgetalmax.ToString();
+                    Schaalfactor.Text = schaalfactor.ToString();
+
+                    break;
+
+                case "neon":
+
+                    xmidden = -1.1532226562;
+                    ymidden = 0.3066015625;
+                    schaal = 0.000009765625;
+                    mandelgetalmax = 300;
+                    schaalfactor = 0.5;
+
+                    kleur1 = Color.Black;
+                    kleur2 = Color.Aqua;
+                    kleur3 = Color.BlueViolet;
+                    kleur4 = Color.HotPink;
+                    kleur5 = Color.Aquamarine;
+                    kleur6 = Color.Lime;
+                    kleur7 = Color.Cyan;
+
+                    MiddenX.Text = xmidden.ToString();
+                    MiddenY.Text = ymidden.ToString();
+                    Schaal.Text = schaal.ToString();
+                    Max.Text = mandelgetalmax.ToString();
+                    Schaalfactor.Text = schaalfactor.ToString();
+
+                    break;
+
+                case "tunnel":
+
+                    xmidden = 0.43820090888;
+                    ymidden = -0.2505415793;
+                    schaal = 0.0000000000745;
+                    mandelgetalmax = 100;
+                    schaalfactor = 0.5;
+
+
+                    kleur1 = Color.Navy;
+                    kleur2 = Color.LimeGreen;
+                    kleur3 = Color.LimeGreen;
+                    kleur4 = Color.Yellow;
+                    kleur5 = Color.Red;
+                    kleur6 = Color.Red;
+                    kleur7 = Color.HotPink;
+                    kleur8 = Color.Yellow;
+
+                    MiddenX.Text = xmidden.ToString();
+                    MiddenY.Text = ymidden.ToString();
+                    Schaal.Text = schaal.ToString();
+                    Max.Text = mandelgetalmax.ToString();
+                    Schaalfactor.Text = schaalfactor.ToString();
+
+                    break;
+
+                case "kristal":
+                    xmidden = -0.5470214843;
+                    ymidden = 0.62149902343;
+                    schaal = 0.0000048828125;
+                    mandelgetalmax = 100;
+                        schaalfactor = 0.5;
+
+                    kleur1 = Color.Black;
+                    kleur2 = Color.LightCyan;
+                    kleur3 = Color.LightCyan;
+                    kleur4 = Color.PowderBlue;
+                    kleur5 = Color.PowderBlue;
+                    kleur6 = Color.AliceBlue;
+                    kleur7 = Color.White;
+                    kleur8 = Color.PowderBlue;
+
+                    MiddenX.Text = xmidden.ToString();
+                    MiddenY.Text = ymidden.ToString();
+                    Schaal.Text = schaal.ToString();
+                    Max.Text = mandelgetalmax.ToString();
+                    Schaalfactor.Text = schaalfactor.ToString();
+
+                    break;
+
+                case "kerstkrans":
+
+                    xmidden = -0.5298514404;
+                    ymidden = 0.61805523681;
+                    schaal = 0.000004150390625;
+                    mandelgetalmax = 100;
+                    schaalfactor = 0.5;
+
+                    kleur1 = Color.Black;
+                    kleur2 = Color.Green;
+                    kleur3 = Color.Green;
+                    kleur4 = Color.Red;
+                    kleur5 = Color.Green;
+                    kleur6 = Color.Green;
+                    kleur7 = Color.Red;
+                    kleur8 = Color.Green;
+
+                    MiddenX.Text = xmidden.ToString();
+                    MiddenY.Text = ymidden.ToString();
+                    Schaal.Text = schaal.ToString();
+                    Max.Text = mandelgetalmax.ToString();
+                    Schaalfactor.Text = schaalfactor.ToString();
+
+                    break;
+            }
+            Invalidate();
+        }
+
+    }
+   
+}
---
+++ mandel/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Mandelbrot
+{
+    
+
+   public partial class MandelForm : Form
+{
+        // membervariabelen
+        PictureBox picturebox;
+        ComboBox Keuzelijst;
+        Button OK;
+        Button Reset;
+        TextBox MiddenX;
+        TextBox MiddenY;
+        TextBox Schaal;
+        TextBox Schaalfactor;
+        TextBox Max;
+
+    public MandelForm()
+    {
+            this.ClientSize = new Size(460, 535);
+            this.BackColor = Color.WhiteSmoke;
+            this.Text = "Mandelbrot";
+            this.Paint += TekenScherm;
+            
+            // picturebox
+            picturebox = new PictureBox();
+            picturebox.Paint += Tekenmandel;
+            picturebox.Location = new Point(30, 110);
+            picturebox.Size = new Size(400, 400);
+            picturebox.BackColor = Color.Black;
+            Controls.Add(picturebox);
+            picturebox.Click += Klikzoom;
+
+            // combobox
+            Keuzelijst = new ComboBox();
+            Keuzelijst.FormattingEnabled = true;
+            Keuzelijst.Items.AddRange(new object[] { "basis", "neon", "tunnel", "kristal", "kerstkrans" });
+            Keuzelijst.Location = new Point(95, 75);
+            Keuzelijst.Name = "Keuzelijst";
+            Keuzelijst.Text = "basis";
+            Keuzelijst.Size = new Size(80, 20);
+            Keuzelijst.TabIndex = 0;
+            Controls.Add(Keuzelijst);
+            Keuzelijst.SelectedIndexChanged += Keuzelijst_SelectedIndexChanged;
+
+            // buttons
+            OK = new Button
+            {
+                Location = new Point(370, 55),
+                Size = new Size(60, 40),
+                Text = "OK",
+                BackColor = Color.LightGray
+            };
+
+            OK.Click += Knopklik_Click;
+
+            Reset = new Button
+            {
+                Location = new Point(370, 15),
+                Size = new Size(60, 40),
+                Text = "Reset Schaal",
+                BackColor = Color.LightGray
+            };
+
+            Reset.Click += Knopklik_Click;
+
+            // tekstboxen
+            MiddenX = new TextBox
+            {
+                Location = new Point(95, 15),
+                Size = new Size(80, 20),
+                 Text = "0"
+        };
+             MiddenY = new TextBox
+            {
+                Location = new Point(95, 45),
+                Size = new Size(80, 30),
+                Text = "0",
+        };
+             Schaal = new TextBox
+            {
+                Location = new Point(260, 15),
+                Size = new Size(100, 30),
+                 Text = "0,01"
+        };
+            Schaalfactor = new TextBox
+            {
+                Location = new Point(260, 45),
+                Size = new Size(100, 30),
+                Text = "0,5"
+                };
+            Max = new TextBox
+            {
+                Location = new Point(260, 75),
+                Size = new Size(100, 30),
+                Text = "100"
+        };
+
+            this.Controls.Add(OK);
+            this.Controls.Add(Reset);
+            this.Controls.Add(MiddenX);
+            this.Controls.Add(MiddenY);
+            this.Controls.Add(Schaal);
+            this.Controls.Add(Schaalfactor);
+            this.Controls.Add(Max);
+        }
+
+
+    void TekenScherm(object obj, PaintEventArgs pea)
+    {
+            Graphics gr = pea.Graphics;
+
+            // tekst
+
+        gr.DrawString("midden X:"
+            , new Font("Tahoma", 10)
+            , Brushes.Black
+            , 30, 15
+            );
+        gr.DrawString("midden Y:"
+            , new Font("Tahoma", 10)
+            , Brushes.Black
+            , 30, 45
+            );
+        gr.DrawString("schaal:"
+            , new Font("Tahoma", 10)
+            , Brushes.Black
+            , 180, 15
+            );
+            gr.DrawString("schaalfactor:"
+            , new Font("Tahoma", 10)
+            , Brushes.Black
+            , 180, 45
+                );
+            gr.DrawString("max:"
+            , new Font("Tahoma", 10)
+            , Brushes.Black
+            , 180, 75
+            );
+            gr.DrawString("preset:"
+            , new Font("Tahoma", 10)
+            , Brushes.Black
+            , 30, 75
+            );
+    }
+}
+
+
+class Program
+{
+        // main methode
+    static void Main()
+    {
+        // maak new form
+        MandelForm scherm;
+        scherm = new MandelForm();
+        Application.Run(scherm);
+    }
+}
+}
